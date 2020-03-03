7755df07e61f8f26efe109503e87cab0a38fdb09
Samuel Lo <locheukyee@hotmail.com>
2020-03-04 06:20:51 +0800

    update setDisplay for Qt

--- setDisplay.sh
+++ setDisplay.sh
@@ -1,1 +1,8 @@
+#sudo apt-get install libxkbcommon-x11-0
 export DISPLAY=localhost:0.0
+export LIBGL_ALWAYS_INDIRECT=1
+
+#The following packages are not required
+#sudo apt-get install mesa-common-dev
+#sudo apt-get install libglu1-mesa-dev
+#sudo apt-get install mesa-utils
